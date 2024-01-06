5d009fa598ad123ac18f943d13189ad187e7a279
baldpanda <37364932+baldpanda@users.noreply.github.com>
2024-01-06 11:55:55 +0000

    Adding ability to create a post

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
 from extensions import db
+from flask import render_template, url_for, redirect
+from models import Post
+from forms import PostForm
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # SQLite database file
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -12,6 +21,17 @@ from models import User, Post
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/post/new', methods=['GET', 'POST'])
+def create_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
