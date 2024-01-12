f0e1a59088abe33b78d72cf486ebc9e1d3a379e5
baldpanda <37364932+baldpanda@users.noreply.github.com>
2024-01-12 19:10:17 +0000

    Using flake8 to apply some linting and updating the formatting accordingly

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 from extensions import db
-from flask import render_template, url_for, redirect
-from models import Post
 from forms import PostForm
 import os
 
@@ -10,49 +8,54 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'site.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "site.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
-from models import User, Post
+from models import Post  # noqa: E402
 
-@app.route('/')
+
+@app.route("/")
 def home():
     posts = Post.query.all()
-    return render_template('index.html', posts=posts)
+    return render_template("index.html", posts=posts)
 
 
-@app.route('/post/new', methods=['GET', 'POST'])
+@app.route("/post/new", methods=["GET", "POST"])
 def create_post():
     form = PostForm()
     if form.validate_on_submit():
         post = Post(title=form.title.data, content=form.content.data)
         db.session.add(post)
         db.session.commit()
-        return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', form=form)
+        return redirect(url_for("home"))
+    return render_template("create_post.html", title="New Post", form=form)
 
-@app.route('/post/<int:post_id>')
+
+@app.route("/post/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id)
-    return render_template('post.html', post=post)
+    return render_template("post.html", post=post)
 
-@app.route('/post/<int:post_id>/delete', methods=['POST'])
+
+@app.route("/post/<int:post_id>/delete", methods=["POST"])
 def delete_post(post_id):
     post = Post.query.get_or_404(post_id)
     # Add verification here to make sure the current user is the author of the post
     db.session.delete(post)
     db.session.commit()
     # Flash a message or log the deletion
-    return redirect(url_for('home'))
+    return redirect(url_for("home"))
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     # Note that we set the 404 status explicitly
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
