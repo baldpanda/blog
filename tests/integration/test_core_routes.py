ee43285a8d067e551fd86c29073a81d655ca4697
baldpanda <37364932+baldpanda@users.noreply.github.com>
2024-01-25 16:10:32 +0000

    Add in another integration test for getting a blog post

--- tests/integration/test_core_routes.py
+++ tests/integration/test_core_routes.py
@@ -14,3 +14,10 @@ def test_home_page(client):
     """Test the home page route."""
     response = client.get('/blog/')
     assert response.status_code == 200
+
+def test_getting_blog_post(client):
+    """Test getting a blog post."""
+    running_blog_post_route = "/blog/post/1"
+    response = client.get(running_blog_post_route)
+    assert response.status_code == 200
+    assert "Goals 2024 - Running" in str(response.data)
