28a3a14d2beb8f30c35d2112aa62792620f451ab
baldpanda <37364932+baldpanda@users.noreply.github.com>
2024-02-03 08:50:47 +0000

    Remove unused model

--- app/models.py
+++ app/models.py
@@ -2,13 +2,6 @@ from datetime import datetime
 from app.extensions import db
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    # Further fields like password, profile picture, etc.
-
-
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
