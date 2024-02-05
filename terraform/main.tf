3661234e85d7679bf586cf9ae1a8a51b89d8de58
baldpanda <37364932+baldpanda@users.noreply.github.com>
2024-02-05 17:42:00 +0000

    Move out the AMI ID into a variable in the variables.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2_blog" {
-  ami           = "ami-0a3c3a20c09d6f377"  # Use an appropriate AMI for your region
+  ami           =  var.ami_id  # Use an appropriate AMI for your region
   instance_type = "t2.micro"    # Choose an instance type
 
   tags = {
