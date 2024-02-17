c9b6593a03b531ae6bcdb04eb975bc833ddce256
baldpanda <37364932+baldpanda@users.noreply.github.com>
2024-02-17 18:18:20 +0000

    Add page with conversational agent

---
+++ app/running_coach.py
@@ -0,0 +1,17 @@
+import os
+from haystack.agents.memory import ConversationSummaryMemory
+from haystack.agents.conversational import ConversationalAgent
+from haystack.nodes import PromptNode
+
+model_api_key = os.getenv("HF_API_KEY", None)
+
+prompt_node = PromptNode(
+    model_name_or_path="HuggingFaceH4/zephyr-7b-beta", api_key=model_api_key, max_length=256, stop_words=["Human"]
+)
+
+summary_memory = ConversationSummaryMemory(prompt_node)
+
+conversational_agent = ConversationalAgent(prompt_node=prompt_node, memory=summary_memory)
+
+def ask_question(question):
+    return conversational_agent.run(question)
