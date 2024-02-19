ad37c31c0d8d98bd0edf4703daac647e75ef171d
baldpanda <37364932+baldpanda@users.noreply.github.com>
2024-02-19 20:19:16 +0000

    Updating prompt for running coach

--- app/running_coach.py
+++ app/running_coach.py
@@ -7,20 +7,15 @@ model_api_key = os.getenv("HF_API_KEY", None)
 
 running_coach_prompt_template = """
 In the following conversation, a human user interacts with an Running Coach AI Agent.
-The human user poses running related questions, and the AI Agent goes through several steps
-to provide well-informed answers.
-If the AI Agent knows the answer, the response begins with "Final Answer:" on a new line.
-
-If the question the human poses is not running related, the agent must response with
-"the question is not running related" as it is only design for running related queries
 
 The following is the previous conversation between a human and an AI:
 {memory}
 
 AI Agent responses must start with one of the following:
 
+Not Running Related: "the question is not running related" if the query is not running related
 Thought: [AI Agent's reasoning process]
-Final Answer: [final answer to the human user's question]
+Final Answer: [final answer to the human user's question only if it is running related]
 
 The AI Agent should not ask the human user for additional information, clarification, or context.
 
@@ -40,8 +35,8 @@ summary_memory = ConversationSummaryMemory(prompt_node)
 conversational_agent = ConversationalAgent(
     prompt_node=prompt_node,
     memory=summary_memory,
-    prompt_template=running_coach_prompt_template,
-)
+    prompt_template=running_coach_prompt_template
+    )
 
 
 def ask_question(question):
