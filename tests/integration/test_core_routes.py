64a3a86c7dc334b769fb0128f3eb79294f75ef37
baldpanda <37364932+baldpanda@users.noreply.github.com>
2024-03-09 13:40:59 +0000

    Add test for 404 page and fix integration test referencing running goals page

--- tests/integration/test_core_routes.py
+++ tests/integration/test_core_routes.py
@@ -17,7 +17,13 @@ def test_home_page(client):
 
 def test_getting_blog_post(client):
     """Test getting a blog post."""
-    running_blog_post_route = "/blog/post/1"
+    running_blog_post_route = "/blog/post/13"
     response = client.get(running_blog_post_route)
     assert response.status_code == 200
     assert "Goals 2024 - Running" in str(response.data)
+
+def test_404_status_code_returned_if_path_not_known(client):
+    """Should return 404 status code for unknown path."""
+    unknown_route = "/not-valid-path"
+    response = client.get(unknown_route)
+    assert response.status_code == 404
